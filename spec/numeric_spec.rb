fd88b0d67fb483ba66f8f845449a3a79a8031b86
Colin <colin@colinframe.com>
2019-02-21 15:16:54 +0000

    Rubocop cleanup.

--- spec/numeric_spec.rb
+++ spec/numeric_spec.rb
@@ -15,16 +15,16 @@ describe Numeric do
       it { is_expected.to eq(20.0) }
     end
   end
-  
+
   describe '#percent_of(number)' do
     context 'as an integer' do
       subject(:percent_of) { 2.percent_of(10) }
-    
+
       it { is_expected.to eq(0.2) }
     end
     context 'as a float' do
       subject(:percent_of) { 2.0.percent_of(10) }
-    
+
       it { is_expected.to eq(0.2) }
     end
   end
